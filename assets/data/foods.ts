1536f3c586a28b750415f307f2ef7b501f944075
Thibaut Hardouin <thibaut.hardouin33@gmail.com>
2024-09-17 23:05:56 +0200

    after putting the first item

---
+++ assets/data/foods.ts
@@ -0,0 +1,22 @@
+const food_elements = [
+    {
+        id: 1,
+        name: 'Beef',
+        image: 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/0/04/Steak_JE4_BE3.png',
+        co2: 30,
+    },
+    {
+        id: 2,
+        name: 'Pork',
+        image: 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/ee/Cooked_Porkchop_JE4_BE3.png',
+        co2: 7,
+    },
+    {
+        id: 3,
+        name: 'Chicken',
+        image: 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/66/Cooked_Chicken_JE3_BE3.png',
+        co2: 6,
+    },
+];
+
+export default food_elements;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,37 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import food_elements from '../../../assets/data/foods';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+const food_element = food_elements[0]
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>PUTE</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: food_element.image }} style={styles.image}/>
+      <Text style={styles.title}>{food_element.name}</Text>
+      <Text style={styles.title}>{food_element.co2}kg</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 100,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    justifyContent: 'center',
+    alignContent: 'center',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+  }
 });
