b556d866cb8e57f5667174c63340ab93dcbf3537
Thibaut Hardouin <thibaut.hardouin33@gmail.com>
2024-09-18 15:14:27 +0200

    no problem

--- src/components/LesItems.tsx
+++ src/components/LesItems.tsx
@@ -1,15 +1,22 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import food_elements from '../../assets/data/foods';
+import { Food_element } from '../types'
+
+
+const defaultFoodImage = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/b/b4/Herobrine.png/'
+
+type ListeDeBouffePropriétés = {
+  food_element: Food_element;
+}
 
 //Fonction pour afficher 1 item
 //En utilisant la fonction ListeDeBouffe, on envoie 1 info, celle de l'élément à saisir dans la liste de
 //données présentes dans ...assets/data/foods.
 //La fonction utilise cette info pour choisir les éléments à afficher dans le bloc d'item, et renvoie
 //toutes les info nécessaires à l'affichage.
-const ListeDeBouffe = ({ food_element }) => {
+const ListeDeBouffe = ({ food_element }: ListeDeBouffePropriétés) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: food_element.image }} style={styles.image}/>
+      <Image source={{ uri: food_element.image || defaultFoodImage }} style={styles.image}/>
       <Text style={styles.title}>{food_element.name}</Text>
       <Text style={styles.title}>{food_element.co2}kg</Text>
     </View>
